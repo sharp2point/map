0d9b556800d492db0c3ffe03888c186c65631a4b
Evgeny Blinov <sharppoint@yandex.ru>
2023-02-03 21:03:32 +0300

    add template dialog to balloon

--- public/src/index.js
+++ public/src/index.js
@@ -1,4 +1,3 @@
-import { dialogOpen } from "./dialog.js";
 let mapa;
 ymaps.ready(init);
 function init() {
@@ -8,9 +7,19 @@ function init() {
     controls: [],
   });
   mapa.events.add("click", (e) => {
-    mapa.panTo(e.get("coords"), {
-      flying: true,
+    const placemark = new ymaps.Placemark(e.get("coords"), {
+      hintContent: "<div> class='map'>ул. Гашека 9ф</div>",
+      balloonContent: `<div class="dialog">
+        <div class="dialog-header"><h2>Отзыв:</h2></div>
+        <div class="dialog-form"><input class="dialog-text__name input-text" type="text" placeholder="Укажите ваше имя" />
+        <input class="dialog-text__place input-text" type="text" placeholder="Укажите место" />
+        <input class="dialog-text__feed input-text" type="textarea" placeholder="Оставьте отзыв" />
+      <button class="dialog-submit" onClick="(function(){
+        console.log('Close');
+      })()">Добавить</button></div></div>`,
+      balloonPanelMaxMapArea: 0,
     });
-    dialogOpen(e.get("position"));
+    mapa.geoObjects.add(placemark);
+    placemark.balloon.open();
   });
 }
